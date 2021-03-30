55978d8688f3ad1e4893369203929cb0f18ba6e6
TheSebasx11 <sebabalcar18@gmail.com>
2021-03-30 00:30:04 -0500

    Aplicación de hovers
    a

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,12 @@
-const menu = document.querySelector(".primaryNav__menu");
-const button = document.querySelector(".bar__button .button");
+function Navigation() {
+    const menu = document.querySelector(".primaryNav__menu");
+    const button = document.querySelector(".bar__button .button");
 
-button.addEventListener('click', (e) => {
+    // button.addEventListener('click', (e) => {
     if (menu.style.display === 'none') {
         menu.style.display = 'flex';
     } else {
         menu.style.display = 'none';
     }
-});
+    // });
+}
