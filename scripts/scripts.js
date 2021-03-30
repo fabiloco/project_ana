7e2eb6cf9546c5a0ef271119449e5c6c4bd494c3
alucard <faalsaru@gmail.com>
2021-03-29 22:21:43 -0500

    repositorio creado nuevamente por bug en github pages

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".primaryNav__menu");
+const button = document.querySelector(".bar__button .button");
+
+button.addEventListener('click', (e) => {
+    if (menu.style.display === 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+});
