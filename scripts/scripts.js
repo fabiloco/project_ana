cb16b92d8b0c4e3def2b87ae58ee927964fc8a23
alucard <faalsaru@gmail.com>
2021-04-02 02:16:54 -0500

    testimonials mobile completed with animations, some improvements to article__us

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,27 @@ function Navigation() {
     }
     // });
 }
+
+//Testimonials cards
+
+const tButton1 = document.getElementById("testimonial__button1");
+const tButton2 = document.getElementById("testimonial__button2");
+const tButton3 = document.getElementById("testimonial__button3");
+const slides = document.querySelectorAll(".testimonial__card");
+
+tButton1.addEventListener("click", (e) => {
+    document.querySelector(".testimonial__card-active").classList.remove("testimonial__card-active");
+    slides[0].classList.add("testimonial__card-active");
+});
+
+tButton2.addEventListener("click", (e) => {
+    document.querySelector(".testimonial__card-active").classList.remove("testimonial__card-active");
+    slides[1].classList.add("testimonial__card-active");
+});
+
+tButton3.addEventListener("click", (e) => {
+    document.querySelector(".testimonial__card-active").classList.remove("testimonial__card-active");
+    slides[2].classList.add("testimonial__card-active");
+});
+
+console.log(slides[0]);
